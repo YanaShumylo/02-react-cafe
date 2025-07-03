ec3a22750dae425f7e231d788d1591cb873fb614
ShumyloYana <zolotushko1@gmail.com>
2025-07-03 13:29:19 +0300

    new app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 // import React from 'react';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import css from './App.module.css';
 import CafeInfo from '../CafeInfo/CafeInfo';
 import VoteOptions from '../VoteOptions/VoteOptions';
